f749149a3fcbcec57dc20d04d1ef5037f9b9f4cf
Anthonyysilv <anthony.silva@aluno.ifsp.edu.br>
2022-12-06 16:16:46 -0300

    06/12 16:16

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,10 +1,58 @@
-var ul = document.querySelector('nav ul');
-var menuBtn = document.querySelector('.menu-btn i');
+const navbarMenu = document.getElementById("navbar");
+const burgerMenu = document.getElementById("burger");
+const overlayMenu = document.querySelector(".overlay");
 
-function menuShow() {
-  if (ul.classList.contains('open')) {
-    ul.classList.remove('open');
-  }else {
-    ul.classList.add('open');
-  }
-}
+// Show and Hide Navbar Function
+const toggleMenu = () => {
+   navbarMenu.classList.toggle("active");
+   overlayMenu.classList.toggle("active");
+};
+
+// Collapsible Mobile Submenu Function
+const collapseSubMenu = () => {
+   navbarMenu
+      .querySelector(".menu-dropdown.active .submenu")
+      .removeAttribute("style");
+   navbarMenu.querySelector(".menu-dropdown.active").classList.remove("active");
+};
+
+// Toggle Mobile Submenu Function
+const toggleSubMenu = (e) => {
+   if (e.target.hasAttribute("data-toggle") && window.innerWidth <= 992) {
+      e.preventDefault();
+      const menuDropdown = e.target.parentElement;
+
+      // If Dropdown is Expanded, then Collapse It
+      if (menuDropdown.classList.contains("active")) {
+         collapseSubMenu();
+      } else {
+         // Collapse Existing Expanded Dropdown
+         if (navbarMenu.querySelector(".menu-dropdown.active")) {
+            collapseSubMenu();
+         }
+
+         // Expanded the New Dropdown
+         menuDropdown.classList.add("active");
+         const subMenu = menuDropdown.querySelector(".submenu");
+         subMenu.style.maxHeight = subMenu.scrollHeight + "px";
+      }
+   }
+};
+
+// Fixed Resize Window Function
+const resizeWindow = () => {
+   if (window.innerWidth > 992) {
+      if (navbarMenu.classList.contains("active")) {
+         toggleMenu();
+      }
+      if (navbarMenu.querySelector(".menu-dropdown.active")) {
+         collapseSubMenu();
+      }
+   }
+};
+
+// Initialize Event Listeners
+burgerMenu.addEventListener("click", toggleMenu);
+overlayMenu.addEventListener("click", toggleMenu);
+navbarMenu.addEventListener("click", toggleSubMenu);
+window.addEventListener("resize", resizeWindow);
